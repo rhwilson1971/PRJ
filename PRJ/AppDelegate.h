e42e48bef9d35b148c4708dfabdb998d01604184
Reuben H Wilson <reuben.wilson@cyn-reub.com>
2014-01-16 19:32:08 -0600

    Initial Commit

---
+++ PRJ/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PRJ
+//
+//  Created by Reuben Wilson on 1/16/14.
+//  Copyright (c) 2014 wymsii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PRJ/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PRJ
+//
+//  Created by Reuben Wilson on 1/16/14.
+//  Copyright (c) 2014 wymsii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
